199d4b328ed7f007bf7148c8a89b032eaf94275b
alaminyusuf <alaminyusuf131@gmail.com>
2022-06-14 23:58:07 +0100

    Fix: Master head tailwind fix

--- components/MasterHead.tsx
+++ components/MasterHead.tsx
@@ -7,19 +7,20 @@ const Header: FC = () => {
       <div className='flex-1 grid'>
         <div className="bg-[url('../public/invoice.png')]  bg-no-repeat"></div>
 
-        <div className='mx-auto w-eighty text-4xl place-items-center'>
-          <h3>
+        <div className='mx-auto w-eighty text-medium place-items-center'>
+          <div>
             <span className='text-forestgreen capitalize'>withness</span> a
-            lofty <span className='text-forestgreen captalize'>success</span>{' '}
+            lofty <span className='text-forestgreen capitalize'>success</span>{' '}
             with automated{' '}
-            <span className='text-forestgreen captalize'>invoicing</span> system
-          </h3>
+            <span className='text-forestgreen capitalize'>invoicing</span>{' '}
+            system
+          </div>
           <Directives />
         </div>
       </div>
 
       <div className='flex-1'>
-        <div className="bg-[url('../public/career.svg')] h-landing bg-cover"></div>
+        <div className="bg-[url('../public/career.svg')] h-landing bg-cover bg-no-repeat"></div>
       </div>
     </div>
   );
